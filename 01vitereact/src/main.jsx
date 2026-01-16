4c116d1025c406ff9475dc0fc6417ef2e9d1ac1c
Shivpratap Singh <2023ucp1655@mnit.ac.in>
2026-01-16 14:37:00 +0530

    learned to create custom react rendering

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -32,7 +32,7 @@ const reactElement = React.createElement(
     'a',
     {href: 'https://google.com',target: '_blank' },
     'click me to visit google',
-    anotherElement
+    anotherUser
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
