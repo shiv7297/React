9c36f5f0b21d97444d6ec6d16a69f94225e3a693
Shivpratap Singh <2023ucp1655@mnit.ac.in>
2026-02-03 18:15:36 +0530

    Custom Hook done

---
+++ 07currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,16 @@
+import {useEffect, useState} from "react"
+
+
+function useCurrencyInfo(currency){
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(`https://open.er-api.com/v6/latest/${currency.toUpperCase()}`)
+        .then((res) => res.json())
+        .then((res) => setData(res.rates))
+        console.log(data);
+    }, [currency])
+    console.log(data);
+    return data
+}
+
+export default useCurrencyInfo;
